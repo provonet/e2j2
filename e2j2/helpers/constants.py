1df5346a1a45154d2e76cd8e1e29a3ac3b15954b
Johan Bakker <johan.bakker@panifex.nl>
2019-07-23 20:40:36 +0200

    update version

--- e2j2/helpers/constants.py
+++ e2j2/helpers/constants.py
@@ -1,3 +1,4 @@
+VERSION = '0.2.0'
 ERROR = '** ERROR'
 BRIGHT_RED = '\033[1;31m'
 RESET_ALL = '\033[00m'
@@ -6,7 +7,6 @@ GREEN = '\033[0;32m'
 LIGHTGREEN = '\033[1;32m'
 WHITE = '\033[0;37m'
 DESCRIPTION = 'Parse jinja2 templates with enhanced JSON aware environment variables'
-VERSION = '0.1.30'
 VAULT_STATUSCODES = {
     '200': 'Success',
     '204': 'Success, no data',
