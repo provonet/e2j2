db2e5611532ffce6201cc30075645ebb7cea4d22
Johan Bakker <johan.bakker@solvinity.com>
2020-07-28 08:43:21 +0200

    fix upload script for new constants.py location

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf build/*
 rm -rf dist/*
-VERSION=$(grep VERSION e2j2/helpers/constants.py | cut -d "'" -f 2)
+VERSION=$(grep VERSION e2j2/constants.py | cut -d "'" -f 2)
 python setup.py sdist bdist_wheel
 gpg --detach-sign -a dist/e2j2-${VERSION}.tar.gz
 twine upload dist/e2j2-${VERSION}.tar.gz dist/e2j2-${VERSION}-py3-none-any.whl dist/e2j2-${VERSION}.tar.gz.asc
